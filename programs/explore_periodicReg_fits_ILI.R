b99a54fe18e0d0017e39e465bbeb4604b790ea74
Elizabeth Lee <eclee25@gmail.com>
2015-10-20 12:37:20 -0400

    add diagnostic plots for regression
    explore_periodicReg_fits_ILI.R
    - residuals (observed - fitted) vs fitted scatterplots for each zip3
    - color points if the observed datum corresponding with the residual
    falls in the 95% CI of the model fit
    - findings: heteroscedastic, 95% of data do not fit within 95% CI of
    model fit

--- programs/explore_periodicReg_fits_ILI.R
+++ programs/explore_periodicReg_fits_ILI.R
@@ -31,24 +31,44 @@ setwd('../R_export')
 data <- read_csv(file=sprintf('periodicReg_%sallZip3Mods_ILI%s.csv', code, code2), col_types=list(ili=col_double(), .fitted=col_double(), .se.fit=col_double()))
 
 #### initial time series plots ################################
-dir.create(sprintf('../graph_outputs/explore_periodicReg_%sfits_ILI%s', code, code2), showWarnings=FALSE)
+dir.create(sprintf('../graph_outputs/explore_periodicReg_%sfits_ILI%s', code, code2), showWarnings = FALSE)
 setwd(sprintf('../graph_outputs/explore_periodicReg_%sfits_ILI%s', code, code2))
 
 zip3list <- data %>% filter(incl.lm) %>% select(zip3) %>% distinct %>% mutate(for.plot = seq_along(1:nrow(.))) %>% mutate(zipname = substr.Right(gsub("X", "00", zip3), 3))
 data_plot <- right_join(data, zip3list, by="zip3")
 indexes <- seq(1, max(data_plot %>% select(for.plot)), by=6)
 
-for(i in indexes){
-  dummyplots <- ggplot(data_plot %>% filter(for.plot>= i & for.plot < i+6), aes(x=week, y=ili, group=zipname)) +
+# for(i in indexes){
+#   dummyplots <- ggplot(data_plot %>% filter(for.plot>= i & for.plot < i+6), aes(x=week, y=ili, group=zipname)) +
+#     theme(axis.text=element_text(size=12), axis.title=element_text(size=14,face="bold")) +
+#     geom_line(aes(color = flu.week)) + scale_color_discrete(name='flu.week (fit: Apr to Oct)') + 
+#     geom_line(aes(y = .fitted), color = 'black') + 
+#     geom_ribbon(aes(ymin = .fitted-(1.96*.se.fit), ymax = .fitted+(1.96*.se.fit), alpha=0.7), fill = 'green') +
+#     scale_alpha_continuous(name='', breaks=c(0.7), labels=c('95% CI fit')) + 
+#     facet_wrap(~zipname, scales="free_y")
+#   # grab zip3s in plot for file name
+#   ziplabels <- data_plot %>% select(zipname) %>% distinct %>% slice(c(i, i+5)) 
+#   ggsave(sprintf("periodicReg_%sfits_ILI%s_%s-%s.png", code, code2, ziplabels[1,], ziplabels[2,]), dummyplots, width=w, height=h)
+# } # saved 10/19/15
+
+#### 10/20/15 residual vs fitted ################################
+dir.create('./diagnostics', showWarnings = FALSE)
+setwd('./diagnostics')
+
+data_plot2 <- data_plot %>% filter(fit.week) %>% mutate(resid = ili - .fitted) %>% mutate(data95indic = ifelse(ili > .fitted-(1.96*.se.fit) & ili < .fitted+(1.96*.se.fit), TRUE, FALSE))
+indexes2 <- indexes
+
+for (i in indexes2){
+  dummyplots <- ggplot(data_plot2 %>% filter(for.plot>= i & for.plot < i+6), aes(x=.fitted, y=resid, group=zipname)) +
+    geom_point(aes(colour = data95indic)) +
+    scale_colour_discrete(name = 'In 95%CI of model fit') + # Do 95% of observed data fit within the 95%CI?
     theme(axis.text=element_text(size=12), axis.title=element_text(size=14,face="bold")) +
-    geom_line(aes(color = flu.week)) + scale_color_discrete(name='flu.week (fit: Apr to Oct)') + 
-    geom_line(aes(y = .fitted), color = 'black') + 
-    geom_ribbon(aes(ymin = .fitted-(1.96*.se.fit), ymax = .fitted+(1.96*.se.fit), alpha=0.7), fill = 'green') +
-    scale_alpha_continuous(name='', breaks=c(0.7), labels=c('95% CI fit')) + 
-    facet_wrap(~zipname, scales="free_y")
-  # grab zip3s in plot for file name
-  ziplabels <- data_plot %>% select(zipname) %>% distinct %>% slice(c(i, i+5)) 
-  ggsave(sprintf("periodicReg_%sfits_ILI%s_%s-%s.png", code, code2, ziplabels[1,], ziplabels[2,]), dummyplots, width=w, height=h)
-} # saved 10/19/15
+    facet_wrap(~zipname, scales="free") +
+    xlab('Fitted Values (fit.week = T)') +
+    ylab('Residuals (obs - fitted)')
+    # grab zip3s in plot for file name
+    ziplabels <- data_plot %>% select(zipname) %>% distinct %>% slice(c(i, i+5)) 
+    ggsave(sprintf("periodicReg_%sresidualsVsFitted_ILI%s_%s-%s.png", code, code2, ziplabels[1,], ziplabels[2,]), dummyplots, width=w, height=h)
+} # saved 10/20/15
 
 
