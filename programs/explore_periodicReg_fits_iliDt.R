94ac545da95a939713f78ffe6fd33b36bafb651c
Elizabeth Lee <eclee25@gmail.com>
2015-10-26 10:48:11 -0400

    rename plots
    explore_periodicReg_fits_iliDt.R
    - explore plot names were renamed from "ILI" to "iliDt"
    - replot for span 0.4 & 0.6

--- programs/explore_periodicReg_fits_iliDt.R
+++ programs/explore_periodicReg_fits_iliDt.R
@@ -22,12 +22,13 @@ code <-"" # linear time trend term
 # code2 <- "_Oct" # fluseason = Oct to Apr
 code2 <- "_Octfit" # fit = Apr to Oct and fluseason = Oct to Apr
 
-span.var <- 0.4 # 0.4, 0.6
+span.var <- 0.6 # 0.4, 0.6
 degree.var <- 2
 code.str <- sprintf('_span%s_degree%s', span.var, degree.var)
 
 #### plot formatting ################################
 w <- 9; h <- 6
+num <- 6 # number of figures per plot
 
 #### import data ################################
 setwd('../R_export')
@@ -39,10 +40,10 @@ setwd(sprintf('../graph_outputs/explore_periodicReg_%sfits_iliDt%s%s', code, cod
 
 zip3list <- data %>% filter(incl.lm) %>% select(zip3) %>% distinct %>% mutate(for.plot = seq_along(1:nrow(.)))
 data_plot <- right_join(data, zip3list, by="zip3")
-indexes <- seq(1, max(data_plot %>% select(for.plot)), by=6)
+indexes <- seq(1, max(data_plot %>% select(for.plot)), by=num)
 
 for(i in indexes){
-  dummyplots <- ggplot(data_plot %>% filter(for.plot>= i & for.plot < i+6), aes(x=week, y=ili.dt, group=zipname)) +
+  dummyplots <- ggplot(data_plot %>% filter(for.plot>= i & for.plot < i+num), aes(x=week, y=ili.dt, group=zipname)) +
     theme(axis.text=element_text(size=12), axis.title=element_text(size=14,face="bold")) +
     geom_line(aes(color = flu.week)) + scale_color_discrete(name='flu.week (fit: Apr to Oct)') + 
     geom_line(aes(y = .fitted), color = 'black') + 
@@ -50,8 +51,8 @@ for(i in indexes){
     scale_alpha_continuous(name='', breaks=c(0.7), labels=c('95% CI fit')) + 
     facet_wrap(~zipname, scales="free_y")
   # grab zip3s in plot for file name
-  ziplabels <- data_plot %>% select(zipname) %>% distinct %>% slice(c(i, i+5)) 
-  ggsave(sprintf("periodicReg_%sfits_ILI%s%s_%s-%s.png", code, code2, code.str, ziplabels[1,], ziplabels[2,]), dummyplots, width=w, height=h)
+  ziplabels <- data_plot %>% select(zipname) %>% distinct %>% slice(c(i, i+num-1)) 
+  ggsave(sprintf("periodicReg_%sfits_iliDt%s%s_%s-%s.png", code, code2, code.str, ziplabels[1,], ziplabels[2,]), dummyplots, width=w, height=h)
 } # saved 10/20/15
 
 #### 10/20/15 residual vs fitted ################################
@@ -62,7 +63,7 @@ data_plot2 <- data_plot %>% filter(fit.week) %>% mutate(resid = ili.dt - .fitted
 indexes2 <- indexes
 
 for (i in indexes2){
-  dummyplots <- ggplot(data_plot2 %>% filter(for.plot>= i & for.plot < i+6), aes(x=.fitted, y=resid, group=zipname)) +
+  dummyplots <- ggplot(data_plot2 %>% filter(for.plot>= i & for.plot < i+num), aes(x=.fitted, y=resid, group=zipname)) +
     geom_point(aes(colour = data95indic)) +
     scale_colour_discrete(name = 'In 95%CI of model fit') + # Do 95% of observed data fit within the 95%CI?
     theme(axis.text=element_text(size=12), axis.title=element_text(size=14,face="bold")) +
@@ -70,7 +71,7 @@ for (i in indexes2){
     xlab('Fitted Values (fit.week = T)') +
     ylab('Residuals (obs - fitted)')
     # grab zip3s in plot for file name
-    ziplabels <- data_plot %>% select(zipname) %>% distinct %>% slice(c(i, i+5)) 
+    ziplabels <- data_plot %>% select(zipname) %>% distinct %>% slice(c(i, i+num-1)) 
     ggsave(sprintf("periodicReg_%sresidualsVsFitted_iliDt%s%s_%s-%s.png", code, code2, code.str, ziplabels[1,], ziplabels[2,]), dummyplots, width=w, height=h)
 } # saved 10/20/15
 
