2d949f8a11c3a66f1d13dce757771664a6f81c04
Elizabeth Lee <eclee25@gmail.com>
2015-11-20 13:24:00 -0500

    ts SAIPE income cty
    EDA_income_SAIPE_cty.R
    - plot ts of median household income by county
    grouped by state from SAIPE data
    - renamed choropleth figures and added folders for
    ts and choro

--- programs/EDA_income_SAIPE_cty.R
+++ programs/EDA_income_SAIPE_cty.R
@@ -1,6 +1,6 @@
 ## Name: Elizabeth Lee
 ## Date: 11/2/15
-## Function: EDA - SAIPE median household income - choropleth, county-level
+## Function: EDA - SAIPE median household income - choropleth, county-level; time series by county grouped by state
 ### disease burden metrics: total ili summed across epidemic weeks, cumulative difference in ili and baseline, cumulative difference in ili and epidemic threshold, rate of ili at epidemic peak, epidemic duration, weeks from Nov1 to epidemic start, weeks from epidemic start to peak ili week 
 
 
@@ -21,7 +21,11 @@ setwd(dirname(sys.frame(1)$ofile))
 incVec <- paste("Tier", 1:5)
 colVec <- brewer.pal(length(incVec), 'RdYlGn')
 h <- 5; w <- 8; dp <- 450
+h2 <- 9; w2 <- 9
 mar <- rep(0, 4)
+par(mar = mar, oma = mar)
+years <- c(1995, 1997:2013)
+num <- 6
 
 #### import data ################################
 setwd('../reference_data')
@@ -37,13 +41,20 @@ fullDat <- left_join(incomeDat, abbrDat, by = c("state_id" = "FIPS")) %>%
   mutate(cty = tolower(substring(name, 1, nchar(name)-12))) %>%
   mutate(match = paste(tolower(State), cty, sep=','))
 
+uqst <- fullDat %>% select(State) %>% distinct %>% arrange(State) %>%
+  mutate(for.plot = seq_along(1:nrow(.)))
+
+fullDat2 <- left_join(fullDat, uqst, by = "State")
+indexes <- seq(1, max(fullDat2 %>% select(for.plot)), by=num)
+
+#### plot setup ################################
 setwd(dirname(sys.frame(1)$ofile))
 dir.create("../graph_outputs/EDA_income_SAIPE_cty", showWarnings = FALSE)
 setwd("../graph_outputs/EDA_income_SAIPE_cty")
 
-par(mar = mar, oma = mar)
-years <- c(1995, 1997:2013)
-
+#### choropleths ################################
+dir.create("./choro", showWarnings = FALSE)
+setwd("./choro")
 for (yr in years){
   pltDat <- fullDat %>%
     filter(year == yr) %>%
@@ -55,11 +66,31 @@ for (yr in years){
   pltOrder <- pltDat$county_id[match(map("county", plot=FALSE)$names, pltDat$match)]
   colOrder <- pltDat$inc_col_string[match(pltOrder, pltDat$county_id)]
   
-  png(filename = sprintf("medHouseholdIncome_cty_%s.png", yr), height = h, width = w, units = "in", res = dp)
+  png(filename = sprintf("medHouseholdIncome_SAIPE_cty_%s.png", yr), height = h, width = w, units = "in", res = dp)
   map("county", fill = TRUE, col = colOrder, xlim = c(-124, -65), ylim = c(20, 50), lwd = 0.1)
   legend(list(x = -122.5, y = 24), legend = incVec, horiz = TRUE,  fill = levels(pltDat$income_color), cex = 0.75) # Tier 1= 120K to 140K income
   dev.off()
   print(pltDat %>% count(mi_bin))
-}
+} # renamed 11/20/15
 
 # View(incomeDat %>% filter(name == 'Washington County (ME)'))
+
+#### time series ################################
+dir.create("../ts", showWarnings = FALSE)
+setwd("../ts")
+
+for(i in indexes){
+    dummyplots <- ggplot(fullDat2 %>% filter(for.plot>= i & for.plot < i+num), aes(x=year, y=med_income)) +
+      theme_bw()+
+      theme(axis.text=element_text(size=12), axis.title=element_text(size=14,face="bold")) +
+      geom_line(aes(colour = name)) +
+      geom_point(aes(colour = name)) +
+      scale_y_continuous(name = "Median Household Income") +
+      guides(colour = "none") +
+      coord_cartesian(xlim = c(1995, 2013)) +
+      facet_wrap(~State)
+    labs <- fullDat2 %>% filter(for.plot>= i & for.plot < i+num) %>% select(Abbreviation) %>% distinct %>% slice(c(i, i+num-1))  %>% unlist
+    ggsave(sprintf("medHouseholdIncome_SAIPE_cty_%s-%s.png", labs[1], labs[2]), dummyplots, width = w2, height = h2, dpi = dp)
+} # saved 11/20/15
+
+
