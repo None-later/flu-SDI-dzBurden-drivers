0cf1d9cf1538b27c10686956484f98c65334cee4
Elizabeth Lee <eclee25@gmail.com>
2016-04-26 17:00:21 -0400

    state results & diag after varSelection
    source_prepare_inlaData_st.R
    - model4a_iliSum_v1 function: include only variables that have been selected
    
    source_export_inlaData_st.R
    - fc plot_coefDistr_season function: summarize all coefficients for fixed
    and random effects (separate plots) across models for each season; plot
    mode and 95% CI
    - export_summaryStats, export_summaryStats_rdmOnly, export_summaryStats_fitted
    functions: add modCodeStr, dbCodeStr, season, and exportDate
    
    inla_model4a_iliSum_v1.R
    - clean up and add new path strings
    - save CPO with DIC data
    - calculate logyhat residuals and add to plot dataframe
    - reorganize diagnostic figures
    - export summary statistics for all marginal posteriors
    - plot coefficient distributions by season

--- programs/source_export_inlaData_st.R
+++ programs/source_export_inlaData_st.R
@@ -43,7 +43,6 @@ plot_state_choropleth <- function(exportPath, pltDat, pltVarTxt, code){
   ggsave(exportPath, choro, height = h, width = w, dpi = dp)  
   
 }
-
 ################################
 
 plot_fixedFx_marginals <- function(exportPath, modelOutput, modCodeStr, s){
@@ -66,7 +65,6 @@ plot_fixedFx_marginals <- function(exportPath, modelOutput, modCodeStr, s){
   }
   
 }
-
 ################################
 
 plot_rdmFx_summary_map <- function(exportPath, pltDat, rdmFxTxt){
@@ -100,6 +98,50 @@ plot_rdmFx_summary_map <- function(exportPath, pltDat, rdmFxTxt){
   ggsave(exportPath, summary.map, height = h, width = w, dpi = dp) 
   
 }
+################################
+
+plot_coefDistr_season <- function(path_csvExport, path_plotExport_coefDistr){
+  # plot all coef modes, Q.025 - Q0.975 over time
+  print(match.call())
+  
+  # plot formatting
+  w <- 8; h <- 8; dp <- 250
+  
+  # grab list of files names
+  setwd(path_csvExport)
+  readfile_list <- grep("summaryStats_", list.files(), value = TRUE)
+  coefDf <- tbl_df(data.frame(modCodeStr = c(), dbCodeStr = c(), season = c(), RV = c(), mean = c(), sd = c(), q_025 = c(), q_5 = c(), q_975 = c(), mode = c()))
+
+  for (infile in readfile_list){
+    seasFile <- read_csv(infile, col_types = "cci_cdddddd_")
+    coefDf <- bind_rows(coefDf, seasFile)
+  }
+ 
+  # separate plots with random and fixed effects 
+  names_fixedFx <- coefDf %>% select(RV) %>% unique %>% unlist
+  names_fixedFx_cl <- grep("[OX]{1}[_]{1}", unlist(strsplit(names_fixedFx, split = "[()]")), value = TRUE)
+  coefDf_fixed <- coefDf %>% filter(RV %in% names_fixedFx_cl)
+  coefDf_random <- coefDf %>% filter(!(RV %in% names_fixedFx_cl))
+  
+  # plot fixed effects
+  fixedFig <- ggplot(coefDf_fixed, aes(x = season, y = mode, group = RV)) +
+    geom_pointrange(aes(ymin = q_025, ymax = q_975)) +
+    geom_hline(yintercept = 0) +
+    facet_wrap(~RV, scales = "free_y") +
+    ylab("coefMode (95%CI)") +
+    xlim(c(1, 10))
+  ggsave(paste0(path_plotExport_coefDistr, "fixed.png"), fixedFig, height = h, width = w, dpi = dp)
+  
+  # plot random effects
+  rdmFig <- ggplot(coefDf_random, aes(x = season, y = mode, group = RV)) +
+    geom_pointrange(aes(ymin = q_025, ymax = q_975)) +
+    geom_hline(yintercept = 0) +
+    facet_wrap(~RV, scales = "free_y") +
+    ylab("coefMode (95%CI)") +
+    xlim(c(1, 10))
+  ggsave(paste0(path_plotExport_coefDistr, "random.png"), rdmFig, height = h, width = w, dpi = dp)
+ 
+}
 
 #### functions for data processing prior to export  ################################
 
@@ -128,7 +170,7 @@ plot_rdmFx_summary_map <- function(exportPath, pltDat, rdmFxTxt){
 
 #### functions for data export  ################################
 
-export_summaryStats <- function(exportPath, modelOutput, rdmFxTxt){
+export_summaryStats <- function(exportPath, modelOutput, rdmFxTxt, modCodeString, dbCodeString, season){
   # export summary statistics of INLA model output -- fixed and random effects in the same file
   print(match.call())
 
@@ -145,7 +187,9 @@ export_summaryStats <- function(exportPath, modelOutput, rdmFxTxt){
     select(RV, mean, sd, q_025, q_5, q_975, mode, kld)
   
   # bind data together
-  summaryStats <- bind_rows(summaryFixed, summaryRandom)
+  summaryStats <- bind_rows(summaryFixed, summaryRandom) %>%
+    mutate(modCodeStr = modCodeString, dbCodeStr = dbCodeString, season = season, exportDate = as.character(Sys.Date())) %>%
+    select(modCodeStr, dbCodeStr, season, exportDate, RV, mean, sd, q_025, q_5, q_975, mode, kld)
   
   # export data to file
   write_csv(summaryStats, exportPath)
@@ -154,7 +198,7 @@ export_summaryStats <- function(exportPath, modelOutput, rdmFxTxt){
 
 ################################
 
-export_summaryStats_rdmOnly <- function(exportPath, modelOutput, rdmFxTxt){
+export_summaryStats_rdmOnly <- function(exportPath, modelOutput, rdmFxTxt, modCodeString, dbCodeString, season){
   # export summary statistics of INLA model output -- only random effects in the same file
   print(match.call())
   
@@ -163,34 +207,39 @@ export_summaryStats_rdmOnly <- function(exportPath, modelOutput, rdmFxTxt){
   
   # clean random effects summary statistics output from INLA
   summaryRandom <- tbl_df(modelOutput$summary.random$ID) %>%
-    mutate(RV = paste0(rdmFxTxt, ID)) %>%
-    select(RV, mean, sd, q_025, q_5, q_975, mode, kld)
+    mutate(modCodeStr = modCodeString, dbCodeStr = dbCodeString, season = season, exportDate = as.character(Sys.Date()), RV = paste0(rdmFxTxt, ID)) %>%
+    select(modCodeStr, dbCodeStr, season, exportDate, RV, mean, sd, q_025, q_5, q_975, mode, kld)
   
   # export data to file
   write_csv(summaryRandom, exportPath)
   
 }
-
-export_summaryStats_fitted <- function(exportPath, modelOutput){
+export_summaryStats_fitted <- function(exportPath, modelOutput, residualsDf, modCodeString, dbCodeString, season){
   # export summary statistics of INLA fitted values
   print(match.call())
   
   # variable name output from INLA
-  names(modelOutput$summary.fitted.values) <- c("mean", "sd", "q_025", "q_5", "q_975", "mode")
+  names(modelOutput$summary.fitted.values) <- c("mn", "sd", "q_025", "q_5", "q_975", "mode")
   idvar <- paste0("yhat", as.character(as.numeric(substr.Right(rownames(modelOutput$summary.fitted.values), 2))))
   
   # clean summary statistics output for fitted values (yhat)
-  summaryFitted <- tbl_df(modelOutput$summary.fitted.values) %>%
-    mutate(ID = idvar) %>%
-    select(ID, mean, sd, q_025, q_5, q_975, mode)
+  summaryFitted_save <- tbl_df(modelOutput$summary.fitted.values) %>%
+    mutate(modCodeStr = modCodeString, dbCodeStr = dbCodeString, season = season, exportDate = as.character(Sys.Date()), ID = idvar) %>%
+    select(modCodeStr, dbCodeStr, season, exportDate, ID, mn, sd, q_025, q_5, q_975, mode) %>%
+    mutate(logy = residualsDf$logy, logyhat_resid = residualsDf$residVec) %>%
+    mutate(yhat_mn = exp(mn), yhat_sd = exp(sd), yhat_mode = exp(mode)) 
   
+  summaryFitted_obj <- summaryFitted_save %>%
+    mutate(ID = as.numeric(substring(ID, 5, nchar(ID))))
+    
   # export data to file
-  write_csv(summaryFitted, exportPath)
-  return(summaryFitted)
+  write_csv(summaryFitted_save, exportPath)
+  return(summaryFitted_obj)
 }
 
 export_DIC <- function(exportPath, dicDataframe){
-  # export DIC values across all seasons for a given model set
+  # export DIC & CPO values across all seasons for a given model set
+  # if cpoFail > 0, CPO calculation is invalid
   print(match.call())
   
   # parse modCodeStr
@@ -199,7 +248,7 @@ export_DIC <- function(exportPath, dicDataframe){
   # clean data frame
   dicOutput <- tbl_df(dicDataframe) %>%
     mutate(modCode = parsed[1], dbMetric = parsed[2], version = parsed[3]) %>%
-    select(modCodeStr, modCode, dbMetric, version, season, exportDate, DIC)
+    select(modCodeStr, modCode, dbMetric, version, season, exportDate, DIC, CPO, cpoFail)
   
   if(dim(dicOutput)[1] == 8){
     # export data to file
