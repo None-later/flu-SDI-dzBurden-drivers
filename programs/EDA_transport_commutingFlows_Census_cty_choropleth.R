d3ca7bf44e774c89b10b993f2f3e8f9f2bae1f78
Elizabeth Lee <eclee25@gmail.com>
2016-05-23 19:17:15 -0400

    Census90 commut choro fix
    EDA_transport_commutingFlows_Census_cty_choropleth.R
    - replot choro for Census1990 commuting inflow due to minor typo
    - updates to choroplethr and choroplethrMaps mask dplyr and tidyr functions

--- programs/EDA_transport_commutingFlows_Census_cty_choropleth.R
+++ programs/EDA_transport_commutingFlows_Census_cty_choropleth.R
@@ -12,18 +12,18 @@
 
 #### header #################################
 rm(list = ls())
-require(tidyr)
+require(dplyr); require(tidyr)
 require(ggplot2)
-require(dplyr)
-require(choroplethrMaps)
+require(readr)
   
 setwd(dirname(sys.frame(1)$ofile)) # only works if you source the program
 
 #### read data ################################
-setwd("../../../../Sandra Goldlust's Work/Shared_Data/SG_covariate_data/Cleaned_Data")
-str <- c(rep("c", 10), "i", rep("_", 9))
+setwd("../../../../Sandra Goldlust's Work/EL_SG_Shared_Work/mySQL_Tables/transport_Census00")
+str <- c(rep("c", 10), "i")
 transDat00 <- read_csv("clean_transport_Census00.csv", col_types = paste0(str, collapse=''))
-str <- c(rep("c", 10), "icc", rep("_", 27))
+setwd("../transport_Census90")
+str <- c(rep("c", 10), "icc")
 transDat90 <- read_csv("clean_transport_Census90.csv", col_types = paste0(str, collapse=''), na = '\\N')
 
 setwd(dirname(sys.frame(1)$ofile))
@@ -38,7 +38,7 @@ popDat <- read_csv("CO-EST00INT-TOT.csv") %>%
 processDat <- function(transData){
   fullDat <- transData %>%
     mutate(Res_fips = paste0(Res_ST, Res_CO), Wrk_fips = substr.Right(paste0(Wrk_ST, Wrk_CO), 5)) %>%
-    filter(substring(Wrk_ST, 1, 1) == '0') %>%
+    filter(substring(Wrk_ST, 1, 1) == '0') %>% # domestic workplace
     group_by(Wrk_fips) %>%
     summarise(rawvalue = sum(Count, na.rm=T)) %>%
     left_join(popDat, by = c("Wrk_fips" = "fips"))  %>% 
@@ -52,25 +52,32 @@ fullDat90 <- processDat(transDat90)
 
 #### plot formatting ################################
 h <- 5; w <- 8; dp <- 300
+require(choroplethr)
+require(choroplethrMaps)
 
 # #### choropleth ################################
 setwd(dirname(sys.frame(1)$ofile)) # only works if you source the program
 dir.create(sprintf('../graph_outputs/EDA_transport_commutingFlows_Census_cty'), showWarnings=FALSE) # create directory if not exists
 setwd('../graph_outputs/EDA_transport_commutingFlows_Census_cty')
 
-choro00 <- county_choropleth(fullDat00 %>% rename(value = normvalue), legend = "Into Wk county per 1000") 
+choro00 <- county_choropleth(fullDat00 %>% 
+                               dplyr::rename(value = normvalue), legend = "Into Wk county per 1000") 
 ggsave("commutingInflowsNorm_Census_2000_cty.png", choro00, width = w, height = h, dpi = dp)
-choro00r <- county_choropleth(fullDat00 %>% rename(value = rawvalue), legend = "Into Wk county") 
+choro00r <- county_choropleth(fullDat00 %>% 
+                                dplyr::rename(value = rawvalue), legend = "Into Wk county") 
 ggsave("commutingInflows_Census_2000_cty.png", choro00r, width = w, height = h, dpi = dp)
 
-choro90 <- county_choropleth(fullDat90 %>% rename(value = normvalue), legend = "Into Wk county per 1000") 
+choro90 <- county_choropleth(fullDat90 %>% 
+                               dplyr::rename(value = normvalue), legend = "Into Wk county per 1000") 
 ggsave("commutingInflowsNorm_Census_1990_cty.png", choro90, width = w, height = h, dpi = dp)
-choro90r <- county_choropleth(fullDat00 %>% rename(value = rawvalue), legend = "Into Wk county") 
+choro90r <- county_choropleth(fullDat90 %>% 
+                                dplyr::rename(value = rawvalue), legend = "Into Wk county") 
 ggsave("commutingInflows_Census_1990_cty.png", choro90r, width = w, height = h, dpi = dp)
 
 #### scatter bw access & population variables ################################
 scatterPlot <- function(dataset){
-  dummy <- dataset %>% rename(pop = POPESTIMATE2000)
+  dummy <- dataset %>% 
+    dplyr::rename(pop = POPESTIMATE2000)
   scatters <- ggplot(dummy, aes(x = rawvalue,  y = pop)) +
     geom_point(color = 'black') +
     theme_bw(base_size = 12, base_family = "") +
