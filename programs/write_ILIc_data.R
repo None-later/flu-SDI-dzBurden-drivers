643b848080abac8106450f4690d09304c341abab
Elizabeth Lee <eclee25@gmail.com>
2016-07-16 18:51:12 -0400

    change ili & pop = 0 assignment from database
    write_ILIc_data.R
    - fill in zip3 pops if data were missing for 3 years or less;
    pop data from later years was duplicated to fill in previous
    years
    - fill in ILI data if there were no rows in the database
    - data were exported

--- programs/write_ILIc_data.R
+++ programs/write_ILIc_data.R
@@ -7,7 +7,7 @@
 
 ## Filenames: physician_coverage/DX_Coverage_by_Flu_Season_20150620.csv; Py_export/iliByallZip_allWeekly_totServ_totAge.csv
 ## Data Source: IMS Health ili dataset and physician coverage dataset
-## Notes: 
+## Notes: 7/15/16
 ## 
 ## useful commands:
 ## install.packages("pkg", dependencies=TRUE, lib="/usr/local/lib/R/site-library") # in sudo R
@@ -18,6 +18,7 @@ rm(list = ls())
 require(dplyr)
 require(ggplot2)
 require(tidyr)
+require(readr)
 setwd(dirname(sys.frame(1)$ofile))
 
 #### import data ################################
@@ -42,15 +43,112 @@ ili_df2 <- ili_df %>% mutate(Thu.week = as.Date(week+4)) %>%
 ili_gather_df <- gather(ili_df2, zip3, ili, X2:X999, convert=FALSE) %>% 
   mutate(week = as.Date(week, origin="1970-01-01")) %>%
   mutate(Thu.week = as.Date(Thu.week, origin="1970-01-01")) %>% 
-  mutate(zip3 = substr.Right(sub('X', '00', zip3), 3)) 
+  mutate(zip3 = substr.Right(sub('X', '00', zip3), 3)) %>%
+  mutate(ili = ifelse(is.na(ili), 0, ili)) # 7/15/16: replace NAs with 0
 
 # gather pop data
 pop_gather_df <- gather(pop_df, zip3, pop, X2:X999, convert=FALSE) %>%
   mutate(zip3 = substr.Right(sub('X', '00', zip3), 3)) %>%
   mutate(year = as.numeric(substr(as.character(year), 1, 4)))
 
+#### data cleaning: pop data ####################################
+# clean pop data
+numNAs <- pop_gather_df %>% filter(is.na(pop)) %>% group_by(zip3) %>% count(pop)
+fillZips <- numNAs %>% filter(n <= 3) 
+
+# process zips with 1, 2, and 3 missing pop data differently
+# fill in pop data for locations with only 1-3 population years missing. these populations will be filled in with the same values as subsequent population data (e.g. if zip3 is missing pop data for 2001, fill in with zip3 pop data for 2002)
+
+## 1 NA ##
+fillZips1 <- fillZips %>% filter(n == 1) %>% select(zip3) %>% unlist
+tofill1_df <- pop_gather_df %>%
+  filter(zip3 %in% fillZips1) %>%
+  filter(is.na(pop))
+filler1_df <- tofill1_df %>%
+  mutate(year = year + 1) %>%
+  select(-pop) %>%
+  left_join(pop_gather_df, by = c("year", "zip3")) %>%
+  mutate(year = year - 1)
+
+## 2 NAs ##
+fillZips2 <- fillZips %>% filter(n == 2) %>% select(zip3) %>% unlist
+tofill2_df <- pop_gather_df %>%
+  filter(zip3 %in% fillZips2) %>%
+  filter(is.na(pop))
+# later years will be filled by original pop dataset
+tofill2_df_a <- tofill2_df %>%
+  group_by(zip3) %>%
+  filter(year == max(year))
+filler2_df_a <- tofill2_df_a %>% 
+  mutate(year = year + 1) %>%
+  select(-pop) %>%
+  left_join(pop_gather_df, by = c("year", "zip3")) %>%
+  mutate(year = year - 1)
+# earlier years will be filled by newly filled or original pop dataset
+tofill2_df_b <- tofill2_df %>%
+  group_by(zip3) %>%
+  filter(year == min(year))
+tempPop <- pop_gather_df %>% 
+  filter(!is.na(pop)) %>%
+  bind_rows(filler2_df_a)
+filler2_df_b <- tofill2_df_b %>%
+  mutate(year = year + 1) %>%
+  select(-pop) %>%
+  left_join(tempPop, by = c("year", "zip3")) %>%
+  mutate(year = year - 1)
+# bind the two sets of NAs
+filler2_df <- bind_rows(filler2_df_a, filler2_df_b) %>%
+  arrange(zip3, year)
+
+## 3 NAs ##
+fillZips3 <- fillZips %>% filter(n == 3) %>% select(zip3) %>% unlist
+tofill3_df <- pop_gather_df %>%
+  filter(zip3 %in% fillZips3) %>%
+  filter(is.na(pop))
+# later years will be filled by original pop dataset
+tofill3_df_a <- tofill3_df %>%
+  group_by(zip3) %>%
+  filter(year == max(year))
+filler3_df_a <- tofill3_df_a %>% 
+  mutate(year = year + 1) %>%
+  select(-pop) %>%
+  left_join(pop_gather_df, by = c("year", "zip3")) %>%
+  mutate(year = year - 1)
+# middling years will be filled by newly filled or original pop dataset
+tofill3_df_b <- tofill3_df %>%
+  group_by(zip3) %>%
+  filter(year != min(year) & year != max(year))
+tempPop3 <- pop_gather_df %>% 
+  filter(!is.na(pop)) %>%
+  bind_rows(filler3_df_a)
+filler3_df_b <- tofill3_df_b %>%
+  mutate(year = year + 1) %>%
+  select(-pop) %>%
+  left_join(tempPop3, by = c("year", "zip3")) %>%
+  mutate(year = year - 1)
+# earlier years will be filled by newly filled or original pop dataset
+tofill3_df_c <- tofill3_df %>%
+  group_by(zip3) %>%
+  filter(year == min(year))
+tempPop4 <- tempPop3 %>%
+  bind_rows(filler3_df_b)
+filler3_df_c <- tofill3_df_c %>%
+  mutate(year = year + 1) %>%
+  select(-pop) %>%
+  left_join(tempPop4, by = c("year", "zip3")) %>%
+  mutate(year = year - 1)
+# bind the two sets of NAs
+filler3_df <- bind_rows(filler3_df_a, filler3_df_b, filler3_df_c) %>%
+  arrange(zip3, year)
+
+# merge all pop data #
+pop_data <- pop_gather_df %>%
+  filter(!is.na(pop)) %>%
+  bind_rows(filler3_df, filler2_df, filler1_df) %>%
+  arrange(zip3, year)
+
 # merge ili & pop data
-iliDat <- left_join(ili_gather_df, pop_gather_df, by = c('zip3', 'year'))
+iliDat <- left_join(ili_gather_df, pop_data, by = c('zip3', 'year'))
 
 #### data cleaning: physician coverage data ####################################
 # drop columns in cov data & generate alpha.numer
@@ -82,14 +180,11 @@ alphaDat_Full2 <- arrange(alphaDat_Full2, zip3, year)
 # shorten dataset
 alphaDat <- alphaDat_Full2 %>% select(zip3, year, cov_z.y, alpha_z.y, cov_below5) 
 
-#### data cleaning: generate ilic data ####################################
-# identify zip3s with too little ILI data during fitted week periods (Apr to Oct >= 50 NAs)
-noILIdata <- iliDat %>% filter(fit.week) %>% 
-  filter(Thu.week < as.Date('2009-05-01')) %>% group_by(zip3) %>% 
-  summarise(num.NA = sum(is.na(ili))) %>% mutate(incl.lm = num.NA < 50)
-
-# join datasets to incorporate indicators for including zip3 into lm
-iliDat2 <- right_join(iliDat, (noILIdata %>% select(-num.NA)), by='zip3')
+#### data cleaning: write ilic data ####################################
+# archive: identify zip3s with too little ILI data during fitted week periods (Apr to Oct >= 50 NAs)
+# 7/15/16: identify zip3s with NA as pop; these will be removed from the dataset
+iliDat2 <- iliDat %>%
+  mutate(incl.lm = ifelse(is.na(pop), FALSE, TRUE))
 
 # create ilic metric
 ilicDat <- left_join(iliDat2, alphaDat, by = c("zip3", "year")) %>%
@@ -98,7 +193,7 @@ ilicDat <- left_join(iliDat2, alphaDat, by = c("zip3", "year")) %>%
 #### write Data to file ####################################
 write.csv(alphaDat_Full2, file = 'vizPhysRatio_zipYear_corrections.csv', row.names=F)
 write.csv(ilicDat, file = 'ilicByallZip3_allWeekly_totServ_totAge.csv', row.names=F)
-# exported 10/25/15
+# exported 7/16/16
 
 
 
