bc26d75bf152b4f458fdf71c44dc485172f022e7
Elizabeth Lee <eclee25@gmail.com>
2016-08-20 15:54:41 -0400

    update epi.thresh and incl.analysis
    write_fullIndic_periodicReg_ilinDt.R
    - fix: NOW if ILIn < .fitted, epi.thresh = 0 & epi.week = FALSE
    - change incl.analysis definition to consec.flu.epiweeks >= consec.
    nf.epiweeks instead of just >
    
    string_ILInDt_analyses.R
    - rerun affected functions: changes weeks considered in.season

--- programs/string_ILInDt_analyses.R
+++ programs/string_ILInDt_analyses.R
@@ -58,10 +58,10 @@ for (span in span.list){
   # do.call(write_loess_fits_ILIn, c(params))
   # do.call(explore_loess_fits_ILIn, c(params))
   # do.call(write_periodicReg_fits_ilinDt_Octfit, c(params))
-  # do.call(write_fullIndic_periodicReg_ilinDt, c(params))
+  do.call(write_fullIndic_periodicReg_ilinDt, c(params))
   # do.call(explore_periodicReg_fits_ilinDt, c(params))
-  # do.call(write_relativeDiseaseBurden_ilinDt, c(params))
-  # do.call(explore_dbMetricsDistribution_ilinDt, c(params))
+  do.call(write_relativeDiseaseBurden_ilinDt, c(params))
+  do.call(explore_dbMetricsDistribution_ilinDt, c(params))
   do.call(explore_periodicReg_inSeasonFits_ilinDt, c(params))
 }
 
--- programs/write_fullIndic_periodicReg_ilinDt.R
+++ programs/write_fullIndic_periodicReg_ilinDt.R
@@ -76,7 +76,8 @@ write_fullIndic_periodicReg_ilinDt <- function(span.var, degree.var, spatial){
   data2 <- data %>% mutate(wknum = as.numeric(substr.Right(ISOweek(Thu.week), 2))) %>% mutate(season = ifelse(wknum<40, as.integer(substr(Thu.week, 3, 4)), as.integer(substr(Thu.week, 3, 4))+1)) 
   
   # 1) include only zip3s where lm was performed; 2) set .fitted + 1.96*.se.fit as the epidemic threshold; 3) identify which weeks are epi weeks
-  data3 <- data2 %>% filter(incl.lm) %>% mutate(epi.thresh = .fitted+(1.96*.se.fit)) %>% mutate(epi.week = ilin.dt>epi.thresh)
+  # 8/20/16 if ILIn >= .fitted (incl.lm2), epi.thresh = 0 and epi.week = FALSE
+  data3 <- data2 %>% filter(incl.lm) %>% mutate(epi.thresh = ifelse(incl.lm2, .fitted+(1.96*.se.fit), 0)) %>% mutate(epi.week = ilin.dt>epi.thresh)
   
   ## See explore_fluSeasonDefinition_IR.R for derivation of flu season definition
   # 9/15/15: filter out zip3-season combinations with equivalent or more ILI activity in the previous non-flu season than flu season (season 1 will use subsequent non-flu season)
@@ -86,7 +87,8 @@ write_fullIndic_periodicReg_ilinDt <- function(span.var, degree.var, spatial){
   # 9/15/15 for season 1, use season 1 consec.nf.epiweeks, which occur after the season 1 flu period
   dummy.nf2 <- bind_rows((dummy.nf %>% filter(season==2) %>% ungroup %>% mutate(season=1)), dummy.nf)
   # summarize season-zip3 combinations that have epidemics (num.weeks+ consecutive epidemic weeks)
-  zip3s_with_epi <- full_join(dummy.flu, dummy.nf2, by=c("season", "scale")) %>% mutate(incl.analysis = consec.flu.epiweeks > consec.nf.epiweeks) %>% mutate(has.epi = (consec.flu.epiweeks>=num.weeks))
+  # 8/20/16: incl.analysis = consec.flu.epiweeks >= consec.nf.epiweeks
+  zip3s_with_epi <- full_join(dummy.flu, dummy.nf2, by=c("season", "scale")) %>% mutate(incl.analysis = consec.flu.epiweeks >= consec.nf.epiweeks) %>% mutate(has.epi = (consec.flu.epiweeks>=num.weeks))
   
   # join summary data to full dataset (adds has.epi and incl.analysis indicators)
   data4 <- right_join(data3, zip3s_with_epi %>% select(-contains("consec.")), by=c("season", "scale"))
