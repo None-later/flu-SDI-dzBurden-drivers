0783e2cd410133b3d476d8c431f1ec4ff291f8ed
Elizabeth Lee <eclee25@gmail.com>
2016-08-30 15:33:54 -0400

    6a_debug_v1-1_TX
    inla_model6a_debug_v1.R
    post_inla_model6_oneModcode.R

--- programs/inla_model6a_debug_v1.R
+++ programs/inla_model6a_debug_v1.R
@@ -20,9 +20,9 @@ require(RColorBrewer); require(ggplot2) # export_inlaData_st dependencies
 
 
 #### set these! ################################
-stabbr <- "CA"
+stabbr <- "TX"
 dbCodeStr <- "_ilinDt_Octfit_span0.4_degree2"
-testDataOn <- 2
+testDataOn <- 1
 modCodeStr <- sprintf("6a_debug_v%s-1_%s", testDataOn, stabbr)
 seasons <- 2:9 
 rdmFx_RV <- "nu"
