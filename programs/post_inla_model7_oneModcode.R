fd3c07358864d3107a23d66f601d4fe2a01c3fe7
Elizabeth Lee <eclee25@gmail.com>
2016-10-24 17:12:56 -0400

    change modcodestr
    post_inla_model7_oneModcode.R
    - rerun older modCodeStr to create new scatterplots by season
    (e.g., resid vs fitted, pred vs obs, etc)

--- programs/post_inla_model7_oneModcode.R
+++ programs/post_inla_model7_oneModcode.R
@@ -21,7 +21,7 @@ source("source_export_inlaDiagnostics.R") # plot_diag_scatter_hurdle function, c
 source("source_clean_response_functions_cty.R") # cty response functions
 
 #### set these! ################################
-modCodeStr <- "7d_iliExcessThresh_v3-1"
+modCodeStr <- "7a_iliSum_v3-2"
 seasons <- c(2:9)
 likStrings <- c("gamma")
 
