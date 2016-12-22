1e484f8b92c5544af088aed7a1b6868ec5009617
Elizabeth Lee <eclee25@gmail.com>
2016-12-22 18:42:37 -0500

    model 8a v7-2
    inla_model8a_iliSum_v7.R
    - model 8a V7-2: new prior immunity metric, cty neighbor CAR,
    st flight CAR with no quantile filter

--- programs/inla_model8a_iliSum_v7.R
+++ programs/inla_model8a_iliSum_v7.R
@@ -20,7 +20,7 @@ require(RColorBrewer); require(ggplot2) # export_inlaData_st dependencies
 
 #### set these! ################################
 dbCodeStr <- "_ilinDt_Octfit_span0.4_degree2"
-modCodeStr <- "8a_iliSum_v7-1"; testDataOn <- FALSE
+modCodeStr <- "8a_iliSum_v7-2"; testDataOn <- FALSE
 rdmFx_RV <- "phi"
 likString <- "normal"
 dig <- 4 # number of digits in the number of elements at this spatial scale (~3000 counties -> 4 digits)
