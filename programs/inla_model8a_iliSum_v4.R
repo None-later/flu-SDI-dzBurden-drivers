b77e8f97d762532353952e558aa3c031f44a1c39
Elizabeth Lee <eclee25@gmail.com>
2016-12-25 16:02:51 -0500

    8a V4-3: with interaction imscoverage*careseek
    inla_model8a_iliSum_v4.R
    post_inla_model8_oneModcode.R
    - 8a V4-3: updated priorImmunity and imscoverage x careseek

--- programs/inla_model8a_iliSum_v4.R
+++ programs/inla_model8a_iliSum_v4.R
@@ -20,7 +20,7 @@ require(RColorBrewer); require(ggplot2) # export_inlaData_st dependencies
 
 #### set these! ################################
 dbCodeStr <- "_ilinDt_Octfit_adult_span0.4_degree2"
-modCodeStr <- "8a_iliSum_v4-1"; testDataOn <- FALSE
+modCodeStr <- "8a_iliSum_v4-3"; testDataOn <- FALSE
 rdmFx_RV <- "phi"
 likString <- "normal"
 dig <- 4 # number of digits in the number of elements at this spatial scale (~3000 counties -> 4 digits)
@@ -82,7 +82,7 @@ if (testDataOn){
     f(fips_st_nonzero, model = "iid") + 
     f(regionID_nonzero, model = "iid") + 
     f(season_nonzero, model = "iid") +
-    intercept_nonzero + O_imscoverage_nonzero + O_careseek_nonzero + O_insured_nonzero + X_poverty_nonzero + X_child_nonzero + X_hospaccess_nonzero + X_popdensity_nonzero + X_housdensity_nonzero + X_vaxcovI_nonzero + X_vaxcovE_nonzero + X_H3A_nonzero + X_B_nonzero + X_priorImmunity_nonzero + X_humidity_nonzero + X_pollution_nonzero + X_singlePersonHH_nonzero + X_B_nonzero*X_child_nonzero + offset(logE_nonzero)
+    intercept_nonzero + O_imscoverage_nonzero + O_careseek_nonzero + O_imscoverage_nonzero*O_careseek_nonzero + O_insured_nonzero + X_poverty_nonzero + X_child_nonzero + X_hospaccess_nonzero + X_popdensity_nonzero + X_housdensity_nonzero + X_vaxcovI_nonzero + X_vaxcovE_nonzero + X_H3A_nonzero + X_B_nonzero + X_priorImmunity_nonzero + X_humidity_nonzero + X_pollution_nonzero + X_singlePersonHH_nonzero + X_B_nonzero*X_child_nonzero + offset(logE_nonzero)
 }
 
 #### export formatting ####
@@ -106,30 +106,12 @@ path_csvExport <- getwd()
 modData_full <- modData
 modData_hurdle <- convert_hurdleModel_nz_spatiotemporal(modData_full)
 
-# starting1 <- inla(formula,
-#                   family = "gaussian",
-#                   data = modData_hurdle,
-#                   control.fixed = list(mean = 0, prec = 1/100), # set prior parameters for regression coefficients
-#                   control.predictor = list(compute = TRUE, link = rep(1, nrow(modData_full))),
-#                   control.inla = list(correct = TRUE, correct.factor = 10, diagonal = 1000, strategy = "gaussian", int.strategy = "eb"), # http://www.r-inla.org/events/newfeaturesinr-inlaapril2015; http://www.r-inla.org/?place=msg%2Fr-inla-discussion-group%2Fuf2ZGh4jmWc%2FA0rdPE5W7uMJ
-#                   verbose = TRUE)
-
-# starting2 <- inla(formula,
-#                   family = "gaussian",
-#                   data = modData_hurdle,
-#                   control.fixed = list(mean = 0, prec = 1/100), # set prior parameters for regression coefficients
-#                   control.predictor = list(compute = TRUE, link = rep(1, nrow(modData_full))),
-#                   control.inla = list(correct = TRUE, correct.factor = 10, diagonal = 100, strategy = "gaussian", int.strategy = "eb"), # http://www.r-inla.org/events/newfeaturesinr-inlaapril2015; http://www.r-inla.org/?place=msg%2Fr-inla-discussion-group%2Fuf2ZGh4jmWc%2FA0rdPE5W7uMJ
-#                   control.mode = list(result = starting1, restart = TRUE),
-#                   verbose = TRUE)
-
 starting3 <- inla(formula,
                   family = "gaussian",
                   data = modData_hurdle,
                   control.fixed = list(mean = 0, prec = 1/100), # set prior parameters for regression coefficients
                   control.predictor = list(compute = TRUE, link = rep(1, nrow(modData_full))),
                   control.inla = list(correct = TRUE, correct.factor = 10, diagonal = 10, strategy = "gaussian", int.strategy = "eb"), # http://www.r-inla.org/events/newfeaturesinr-inlaapril2015; http://www.r-inla.org/?place=msg%2Fr-inla-discussion-group%2Fuf2ZGh4jmWc%2FA0rdPE5W7uMJ
-                  # control.mode = list(result = starting2, restart = TRUE),
                   verbose = TRUE)
 
 starting4 <- inla(formula,
