d7321cef8e5563cf1e4ca3ba5e08646217941192
Elizabeth Lee <eclee25@gmail.com>
2017-01-06 21:56:43 -0500

    8a V1-4
    inla_model8a_iliSum_v1.R
    - 8a V1-4
    - analog to V2-5 without spatial CAR terms

--- programs/inla_model8a_iliSum_v1.R
+++ programs/inla_model8a_iliSum_v1.R
@@ -20,7 +20,7 @@ require(RColorBrewer); require(ggplot2) # export_inlaData_st dependencies
 
 #### set these! ################################
 dbCodeStr <- "_ilinDt_Octfit_span0.4_degree2"
-modCodeStr <- "8a_iliSum_v1-3"; testDataOn <- FALSE
+modCodeStr <- "8a_iliSum_v1-4"; testDataOn <- FALSE
 rdmFx_RV <- "nu"
 likString <- "normal"
 dig <- 4 # number of digits in the number of elements at this spatial scale (~3000 counties -> 4 digits)
@@ -45,6 +45,10 @@ path_adjMxExport_cty <- paste0(getwd(), "/US_county_adjacency.graph")
 path_graphIdx_cty <- paste0(getwd(), "/US_county_graph_index.csv")
 path_shape_cty <- paste0(getwd(), "/gz_2010_us_050_00_500k") # for dbf metadata only
 
+setwd('../stateFlightpassenger_graph')
+path_graphExport_st <- paste0(getwd(), "/US_statePassenger_edgelist.txt")
+path_graphIdx_st <- paste0(getwd(), "/US_statePassenger_graph_index.csv")
+
 setwd("../../R_export")
 path_response_cty <- paste0(getwd(), sprintf("/dbMetrics_periodicReg%s_analyzeDB_cty.csv", dbCodeStr))
 
@@ -54,8 +58,9 @@ path_list <- list(path_abbr_st = path_abbr_st,
                   path_shape_cty = path_shape_cty,
                   path_adjMxExport_cty = path_adjMxExport_cty,
                   path_response_cty = path_response_cty, 
-                  path_graphIdx_cty = path_graphIdx_cty)
-
+                  path_graphIdx_cty = path_graphIdx_cty,
+                  path_graphExport_st = path_graphExport_st,
+                  path_graphIdx_st = path_graphIdx_st)
 
 #### MAIN #################################
 #### test data module ####
@@ -71,14 +76,13 @@ if (testDataOn){
     intercept_nonzero + O_imscoverage_nonzero + O_careseek_nonzero + X_poverty_nonzero + offset(logE_nonzero)
 } else{
 #### Import and process data ####
-  modData <- model8a_iliSum_v2(path_list) # with driver & sampling effort variables
+  modData <- model8a_iliSum_v7(path_list) # with driver & sampling effort variables
     #%>%
     # remove_randomObs_stratifySeas(0.4)
   
   formula <- Y ~ -1 + 
     f(ID_nonzero, model = "iid") +
     f(fips_nonzero, model = "iid") + 
-    # f(graphIdx_nonzero, model = "besag", graph = path_adjMxExport_cty) +
     f(fips_st_nonzero, model = "iid") + 
     f(regionID_nonzero, model = "iid") + 
     f(season_nonzero, model = "iid") +
@@ -114,14 +118,14 @@ starting3 <- inla(formula,
                   control.inla = list(correct = TRUE, correct.factor = 10, diagonal = 10, strategy = "gaussian", int.strategy = "eb"), # http://www.r-inla.org/events/newfeaturesinr-inlaapril2015; http://www.r-inla.org/?place=msg%2Fr-inla-discussion-group%2Fuf2ZGh4jmWc%2FA0rdPE5W7uMJ
                   verbose = TRUE)
 
-starting4 <- inla(formula,
-                  family = "gaussian",
-                  data = modData_hurdle,
-                  control.fixed = list(mean = 0, prec = 1/100), # set prior parameters for regression coefficients
-                  control.predictor = list(compute = TRUE, link = rep(1, nrow(modData_full))),
-                  control.inla = list(correct = TRUE, correct.factor = 10, diagonal = 1, strategy = "gaussian", int.strategy = "eb"), # http://www.r-inla.org/events/newfeaturesinr-inlaapril2015; http://www.r-inla.org/?place=msg%2Fr-inla-discussion-group%2Fuf2ZGh4jmWc%2FA0rdPE5W7uMJ
-                  control.mode = list(result = starting3, restart = TRUE),
-                  verbose = TRUE)
+# starting4 <- inla(formula,
+#                   family = "gaussian",
+#                   data = modData_hurdle,
+#                   control.fixed = list(mean = 0, prec = 1/100), # set prior parameters for regression coefficients
+#                   control.predictor = list(compute = TRUE, link = rep(1, nrow(modData_full))),
+#                   control.inla = list(correct = TRUE, correct.factor = 10, diagonal = 1, strategy = "gaussian", int.strategy = "eb"), # http://www.r-inla.org/events/newfeaturesinr-inlaapril2015; http://www.r-inla.org/?place=msg%2Fr-inla-discussion-group%2Fuf2ZGh4jmWc%2FA0rdPE5W7uMJ
+#                   control.mode = list(result = starting3, restart = TRUE),
+#                   verbose = TRUE)
 
 mod <- inla(formula,
             family = "gaussian",
@@ -130,7 +134,7 @@ mod <- inla(formula,
             control.predictor = list(compute = TRUE, link = rep(1, nrow(modData_full))),
             control.compute = list(dic = TRUE, cpo = TRUE),
             control.inla = list(correct = TRUE, correct.factor = 10, diagonal = 0, tolerance = 1e-8), # http://www.r-inla.org/events/newfeaturesinr-inlaapril2015
-            control.mode = list(result = starting4, restart = TRUE),
+            control.mode = list(result = starting3, restart = TRUE),
             verbose = TRUE,
             keep = TRUE, debug = TRUE)
 
