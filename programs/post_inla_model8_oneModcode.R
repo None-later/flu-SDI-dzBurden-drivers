64d23e1e9737bc3f92525316bb93612f123f497b
Elizabeth Lee <eclee25@gmail.com>
2017-01-06 21:54:12 -0500

    8e V2-1
    inla_model8e_epiDur_v2.R
    post_inla_model8_oneModcode.R
    - 8e V2-1
    - epidemic duration - poisson lik
    - added poisson lik option for post_... control flow
    - accidentally called "phi" "nu" in V2-1

--- programs/post_inla_model8_oneModcode.R
+++ programs/post_inla_model8_oneModcode.R
@@ -22,11 +22,11 @@ source("source_export_inlaDiagnostics.R") # plot_diag_scatter_hurdle function
 source("source_clean_response_functions_cty.R") # cty response functions
 
 #### set these! ################################
-dbCodeStr <- "_ilinDt_Octfit_child_span0.4_degree2"
+dbCodeStr <- "_ilinDt_Octfit_span0.4_degree2"
 seasons <- c(3:9)
-modCodeStr <- "8a_iliSum_v3-5"
-likString <- "normal"; likStrings <- c(likString) 
-source("source_calculate_residuals_shift1.R") # calculate_residuals function (source_calculate_residuals_shift1.R for iliSum; source_calculate_residuals.R for epiDur)
+modCodeStr <- "8e_epiDur_v2-1"
+likString <- "poisson"; likStrings <- c(likString) 
+source("source_calculate_residuals.R") # calculate_residuals function (source_calculate_residuals_shift1.R for iliSum; source_calculate_residuals.R for epiDur)
 
 
 #### IMPORT FILEPATHS #################################
@@ -69,7 +69,7 @@ if ("binomial" %in% likStrings){
 }
 
 ### model fit ###
-if ("gamma" %in% likStrings | "normal" %in% likStrings){
+if ("gamma" %in% likStrings | "normal" %in% likStrings | "poisson" %in% likStrings){
   # correlogram: Moran's I vs. distance
   path_plotExport_correlogram <- paste0(path_plotExport, sprintf("/diag_correlog_%s_%s", likString, modCodeStr))
   importPlot_correlogram(path_csvExport, path_plotExport_correlogram, path_list, likString)
