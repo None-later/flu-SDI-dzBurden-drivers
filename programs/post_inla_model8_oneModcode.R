71b87320065f73f7dad96563ec746777f9607016
Elizabeth Lee <eclee25@gmail.com>
2017-01-08 17:04:29 -0500

    8e V1-1
    inla_model8e_epiDur_v1.R
    post_inla_model8_oneModcode.R
    - epiDur model no spatial term
    - fixed lik to poisson after running the model
    - correlogram code breaks post_ program

--- programs/post_inla_model8_oneModcode.R
+++ programs/post_inla_model8_oneModcode.R
@@ -24,7 +24,7 @@ source("source_clean_response_functions_cty.R") # cty response functions
 #### set these! ################################
 dbCodeStr <- "_ilinDt_Octfit_span0.4_degree2"
 seasons <- c(3:9)
-modCodeStr <- "8e_epiDur_v2-1"
+modCodeStr <- "8e_epiDur_v1-1"
 likString <- "poisson"; likStrings <- c(likString) 
 source("source_calculate_residuals.R") # calculate_residuals function (source_calculate_residuals_shift1.R for iliSum; source_calculate_residuals.R for epiDur)
 
