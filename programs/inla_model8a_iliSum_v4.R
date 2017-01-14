03cf765be1be5d975f9f101f265fadb247a065c1
Elizabeth Lee <eclee25@gmail.com>
2017-01-14 10:36:42 -0500

    8a V4-6
    inla_model8a_iliSum_v4.R
    post_inla_model8_oneModcode.R
    - 8a V4-6
    - new standardization across all years for adults, all vars

--- programs/inla_model8a_iliSum_v4.R
+++ programs/inla_model8a_iliSum_v4.R
@@ -20,7 +20,7 @@ require(RColorBrewer); require(ggplot2) # export_inlaData_st dependencies
 
 #### set these! ################################
 dbCodeStr <- "_ilinDt_Octfit_adult_span0.4_degree2"
-modCodeStr <- "8a_iliSum_v4-5"
+modCodeStr <- "8a_iliSum_v4-6"
 rdmFx_RV <- "phi"
 likString <- "normal"
 dig <- 4 # number of digits in the number of elements at this spatial scale (~3000 counties -> 4 digits)
