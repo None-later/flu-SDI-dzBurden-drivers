291858a337b0e91a5ad0105daf315429bd6a82dc
Elizabeth Lee <eclee25@gmail.com>
2017-01-15 10:46:48 -0500

    8a V2-6 m80
    post_inla_model8_oneModcode.R
    - 8a V2-6 m80
    - missing 80%

--- programs/post_inla_model8_oneModcode.R
+++ programs/post_inla_model8_oneModcode.R
@@ -22,11 +22,11 @@ source("source_export_inlaDiagnostics.R") # plot_diag_scatter_hurdle function
 source("source_clean_response_functions_cty.R") # cty response functions
 
 #### set these! ################################
-dbCodeStr <- "_ilinDt_Octfit_child_span0.4_degree2"
+dbCodeStr <- "_ilinDt_Octfit_span0.4_degree2"
 seasons <- c(3:9)
-modCodeStr <- "8e_epiDur_v3-3"
-likString <- "poisson"; likStrings <- c(likString) 
-source("source_calculate_residuals.R") # calculate_residuals function (source_calculate_residuals_shift1.R for iliSum; source_calculate_residuals.R for epiDur)
+modCodeStr <- "8a_iliSum_v2-6_m80"
+likString <- "normal"; likStrings <- c(likString) 
+source("source_calculate_residuals_shift1.R") # calculate_residuals function (source_calculate_residuals_shift1.R for iliSum; source_calculate_residuals.R for epiDur)
 
 
 #### IMPORT FILEPATHS #################################
