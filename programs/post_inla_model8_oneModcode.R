936140f8230ecc2e8607705a4ff09d341f116ba8
Elizabeth Lee <eclee25@gmail.com>
2017-01-17 08:34:39 -0500

    8a V2-6 c75
    post_inla_model8_oneModcode.R
    - rerun 8a V2-6 c75

--- programs/post_inla_model8_oneModcode.R
+++ programs/post_inla_model8_oneModcode.R
@@ -23,7 +23,7 @@ source("source_clean_response_functions_cty.R") # cty response functions
 
 #### set these! ################################
 dbCodeStr <- "_ilinDt_Octfit_span0.4_degree2"
-seasons <- c(3:7)
+seasons <- c(3:9)
 modCodeStr <- "8a_iliSum_v2-6_c75"
 likString <- "normal"; likStrings <- c(likString) 
 source("source_calculate_residuals_shift1.R") # calculate_residuals function (source_calculate_residuals_shift1.R for iliSum; source_calculate_residuals.R for epiDur)
