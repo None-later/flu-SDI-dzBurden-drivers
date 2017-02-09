f02ce5d32e259a00e1ef7c0209b8628c6811c1f1
Elizabeth Lee <eclee25@gmail.com>
2017-02-09 18:00:54 -0500

    updated diag_oneModcode.R for multiseason models
    post_inla_diag_oneModcode.R
    - not run

--- programs/post_inla_diag_oneModcode.R
+++ programs/post_inla_diag_oneModcode.R
@@ -17,22 +17,28 @@ setwd(dirname(sys.frame(1)$ofile))
 source("source_prepare_inlaData_cty.R") # remove_gammaQQ_outliers
 source("source_export_inlaDiagnostics.R") # plot_diag_scatter_hurdle function, calculate_residuals
 source("source_clean_response_functions_cty.R") # cty response functions
-source("source_variableSelection_cty.R") # prepare_allCov_iliSum_cty/_raw
+# source("source_variableSelection_cty.R") # prepare_allCov_iliSum_cty/_raw
 
 #### set these! ################################
 dbCodeStr <- "_ilinDt_Octfit_span0.4_degree2"
-modCodeStr <- "7a_iliSum_v6-1"
+modCodeStr <- "8a_iliSum_v2-6"
 seasons <- c(3:9)
 likString <- "normal"
+source("source_calculate_residuals_shift1.R") # calculate_residuals function (source_calculate_residuals_shift1.R for iliSum; source_calculate_residuals.R for epiDur)
 
 #### IMPORT FILEPATHS #################################
 setwd('../reference_data')
 path_abbr_st <- paste0(getwd(), "/state_abbreviations_FIPS.csv")
 path_latlon_cty <- paste0(getwd(), "/cty_pop_latlon.csv")
-path_adjMxExport_cty <- paste0(getwd(), "/US_county_adjacency_fips.dat")
 
 setwd('./UScounty_shapefiles')
+path_adjMxExport_cty <- paste0(getwd(), "/US_county_adjacency.graph")
+path_graphIdx_cty <- paste0(getwd(), "/US_county_graph_index.csv")
 path_shape_cty <- paste0(getwd(), "/gz_2010_us_050_00_500k") # for dbf metadata only
+
+setwd('../stateFlightpassenger_graph')
+path_graphExport_st <- paste0(getwd(), "/US_statePassenger_edgelist.txt")
+path_graphIdx_st <- paste0(getwd(), "/US_statePassenger_graph_index.csv")
 
 setwd("../../R_export")
 path_response_cty <- paste0(getwd(), sprintf("/dbMetrics_periodicReg%s_analyzeDB_cty.csv", dbCodeStr))
@@ -42,7 +48,10 @@ path_list <- list(path_abbr_st = path_abbr_st,
                   path_latlon_cty = path_latlon_cty,
                   path_shape_cty = path_shape_cty,
                   path_adjMxExport_cty = path_adjMxExport_cty,
-                  path_response_cty = path_response_cty)
+                  path_response_cty = path_response_cty, 
+                  path_graphIdx_cty = path_graphIdx_cty,
+                  path_graphExport_st = path_graphExport_st,
+                  path_graphIdx_st = path_graphIdx_st)
 
 #### EXPORT FILEPATHS #################################
 # predictor plot export directories
@@ -51,9 +60,9 @@ setwd(sprintf("../graph_outputs/inlaModelDiagnostics/%s", modCodeStr))
 dir.create("./diag_predictors", showWarnings = FALSE)
 setwd("./diag_predictors")
 path_plotExport <- getwd()
-dir.create("./rawresid_rawpredictors", showWarnings = FALSE)
-dir.create("./rawresid_stdpredictors", showWarnings = FALSE)
-dir.create("./stdresid_rawpredictors", showWarnings = FALSE)
+# dir.create("./rawresid_rawpredictors", showWarnings = FALSE)
+# dir.create("./rawresid_stdpredictors", showWarnings = FALSE)
+# dir.create("./stdresid_rawpredictors", showWarnings = FALSE)
 dir.create("./stdresid_stdpredictors", showWarnings = FALSE)
 
 # error plot export directories
@@ -77,22 +86,15 @@ setwd(sprintf("../R_export/inlaModelData_export/%s", modCodeStr))
 path_csvExport <- getwd()
 
 #### IMPORT MODEL DATA #################################
-# modData <- prepare_allCov_iliSum_cty(path_list) %>%
-#   remove_case_exceptions(.) %>%
-#   remove_gammaQQ_outliers(.)
-# rmodData <- prepare_allCov_iliSum_cty_raw(path_list) %>%
-#   remove_case_exceptions(.) %>%
-#   remove_gammaQQ_outliers(.)
-modData <- prepare_allCov_logIliSum_cty(path_list) 
-rmodData <- prepare_allCov_logIliSum_cty_raw(path_list) 
+modData <- model8a_iliSum_v7(path_list)
 
 # ### raw residuals vs raw data ####
 # path_plotExport_scatter_rr <- paste0(path_plotExport, "/rawresid_rawpredictors/diag_rawresid_raw")
 # importPlot_diag_scatter_predictors_spatiotemporal(path_csvExport, path_plotExport_scatter_rr, paste0(likString, "_", modCodeStr), "yhat_rawresid", rmodData)
 
-#### raw residuals vs std data ####
-path_plotExport_scatter_rs <- paste0(path_plotExport, "/rawresid_stdpredictors/diag_rawresid_")
-importPlot_diag_scatter_predictors_spatiotemporal(path_csvExport, path_plotExport_scatter_rs, paste0(likString, "_", modCodeStr), "yhat_rawresid", modData)
+# #### raw residuals vs std data ####
+# path_plotExport_scatter_rs <- paste0(path_plotExport, "/rawresid_stdpredictors/diag_rawresid_")
+# importPlot_diag_scatter_predictors_spatiotemporal(path_csvExport, path_plotExport_scatter_rs, paste0(likString, "_", modCodeStr), "yhat_rawresid", modData)
 
 # #### std residuals vs raw data ####
 # path_plotExport_scatter_sr <- paste0(path_plotExport, "/stdresid_rawpredictors/diag_resid_raw")
@@ -114,8 +116,8 @@ importPlot_diag_scatter_ctyerrors_spatiotemporal(path_csvExport, path_plotExport
 path_plotExport_error <- paste0(path_plotExport2, "/diag_")
 importPlot_diag_scatter_errors_spatiotemporal(path_csvExport, path_plotExport_error, likString)
 
-#### vs. index #################################
-path_plotExport_index <- paste0(path_plotExport3, "/diag_")
-importPlot_diag_index_spatiotemporal(path_csvExport, path_plotExport_index, likString)
+# #### vs. index #################################
+# path_plotExport_index <- paste0(path_plotExport3, "/diag_")
+# importPlot_diag_index_spatiotemporal(path_csvExport, path_plotExport_index, likString)
 
 
