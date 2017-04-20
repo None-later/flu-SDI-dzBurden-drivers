706a3f6b17704a2558003a4c7840388808ddee66
Elizabeth Lee <eclee25@gmail.com>
2017-04-20 00:26:34 -0400

    run remaining 2009p data proc steps
    string_ILInDt_analyses.R
    - switch commenting on functions for remaining db functions

--- programs/string_ILInDt_analyses.R
+++ programs/string_ILInDt_analyses.R
@@ -58,6 +58,7 @@ if (spatial.scale == "state"){
   source("write_periodicReg_fits_ilinDt_Octfit_2009p.R")
   source("write_fullIndic_periodicReg_ilinDt_2009p.R")
   source("write_relativeDiseaseBurden_ilinDt_2009p.R")
+  source("explore_periodicReg_inSeasonFits_ilinDt_2009p.R")
 } 
 
 # serv = "_totServ", servToggle = ""
@@ -70,13 +71,13 @@ if (spatial.scale == "state"){
 for (span in span.list){
   params <- list(span.var = span, degree.var = deg, spatial = spatial.params)
 
-  do.call(write_loess_fits_ILIn, c(params))
-  do.call(explore_loess_fits_ILIn, c(params))
-  do.call(write_periodicReg_fits_ilinDt_Octfit, c(params))
-  # do.call(write_fullIndic_periodicReg_ilinDt, c(params))
-  do.call(explore_periodicReg_fits_ilinDt, c(params))
-  # do.call(write_relativeDiseaseBurden_ilinDt, c(params))
-  # do.call(explore_dbMetricsDistribution_ilinDt, c(params))
-  # do.call(explore_periodicReg_inSeasonFits_ilinDt, c(params))
+  # do.call(write_loess_fits_ILIn, c(params))
+  # do.call(explore_loess_fits_ILIn, c(params))
+  # do.call(write_periodicReg_fits_ilinDt_Octfit, c(params))
+  do.call(write_fullIndic_periodicReg_ilinDt, c(params))
+  # do.call(explore_periodicReg_fits_ilinDt, c(params))
+  do.call(write_relativeDiseaseBurden_ilinDt, c(params))
+  do.call(explore_dbMetricsDistribution_ilinDt, c(params))
+  do.call(explore_periodicReg_inSeasonFits_ilinDt, c(params))
 }
 
